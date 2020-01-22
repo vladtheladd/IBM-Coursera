f705cd8adf5ee566e0d8fc7c9ea4b89cd53b5433
unknown <vlad9222@gmail.com>
2020-01-22 16:45:44 -0500

    jupyter notebook project for Week 1

---
+++ jupyter_notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be used for the Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
